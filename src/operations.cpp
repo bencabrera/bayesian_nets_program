643a771830b03a7de786b692b9810ee2eb8e6cfc
Benjamin Cabrera <benny@bcabrera.de>
2018-04-17 15:20:03 +0200

    Implemented and tested all operations

--- src/operations.cpp
+++ src/operations.cpp
@@ -47,18 +47,41 @@ void assert_op(const std::vector<int> places, bool b, JointDist& dist)
 
 void nassert_op(const std::vector<int> places, bool b, JointDist& dist)
 {
+	Marking mask;
+	if(!b)
+		mask.set(); // if b = 0 => set m = 1....1
 
+	for(auto& s : places)
+		mask.flip(s);
+
+	double sum = 0;
+	for(auto& [marking,p] : dist)
+	{
+		auto target = b ? marking | mask : marking & mask;
+		if(target == marking)
+		{
+			sum += p;
+			p = 0;
+		}
+	}
+
+	for(auto& [marking,p] : dist)
+		p = p/(1-sum);
 }
 
-void success_op(const std::vector<int> pre_places, const std::vector<int> post_places, bool b, JointDist& dist)
+void success_op(const std::vector<int> pre_places, const std::vector<int> post_places, JointDist& dist)
 {
+	assert_op(pre_places,1, dist);
+	assert_op(post_places,0, dist);
 
+	set_op(pre_places,0,dist);
+	set_op(post_places,1,dist);
 }
-void fail_pre_op(const std::vector<int> pre_places, const std::vector<int> post_places, bool b, JointDist& dist)
+void fail_pre_op(const std::vector<int> pre_places, const std::vector<int> /*post_places*/, JointDist& dist)
 {
-
+	nassert_op(pre_places, 1, dist);
 }
-void fail_post_op(const std::vector<int> pre_places, const std::vector<int> post_places, bool b, JointDist& dist)
+void fail_post_op(const std::vector<int> /*pre_places*/, const std::vector<int> post_places, JointDist& dist)
 {
-
+	nassert_op(post_places, 0, dist);
 }
--- src/tests/operation_tests.cpp
+++ src/tests/operation_tests.cpp
@@ -68,3 +68,29 @@ TEST_CASE("Assert operation should work correctly 2") {
 
 	compare_dists(updated_dist, expected_dist);
 }
+
+TEST_CASE("Nassert operation should work correctly 1") {
+	std::ifstream f1(TEST_INSTANCE_FOLDER + "operations_initial1.dist");
+	std::ifstream f2(TEST_INSTANCE_FOLDER + "operations_assert1_1.dist");
+
+	auto initial_dist = read_joint_dist(f1); 
+	auto expected_dist = read_joint_dist(f2); 
+
+	auto updated_dist = initial_dist;
+	nassert_op({0}, 0, updated_dist);
+
+	compare_dists(updated_dist, expected_dist);
+}
+
+TEST_CASE("Nassert operation should work correctly 2") {
+	std::ifstream f1(TEST_INSTANCE_FOLDER + "operations_initial1.dist");
+	std::ifstream f2(TEST_INSTANCE_FOLDER + "operations_nassert1_1.dist");
+
+	auto initial_dist = read_joint_dist(f1); 
+	auto expected_dist = read_joint_dist(f2); 
+
+	auto updated_dist = initial_dist;
+	nassert_op({0,2}, 1, updated_dist);
+
+	compare_dists(updated_dist, expected_dist);
+}
